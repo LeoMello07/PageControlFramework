7a5219c9a19f606f31d0dc56f9352a72870c7a6e
Leonardo Mello <leonardomello@MacBook-Air-de-Leonardo.local>
2023-08-06 00:25:52 -0300

    trocando de framework

---
+++ PageControlFramework/BWWalkthroughViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BWWalkthroughViewController.swift
+//  PageControlFramework
+//
+//  Created by Leonardo Mello on 05/08/23.
+//
+
+import UIKit
+
+class WalkthroughViewController: UIViewController {
+
+    @IBOutlet weak var fowardButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    @IBAction func clickNext(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clickBack(_ sender: UIButton) {
+    }
+}
--- Podfile
+++ Podfile
@@ -5,7 +5,8 @@ target 'PageControlFramework' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-	pod "BWWalkthrough"
+	pod 'Instructions', '~> 2.2.0'
+
 
   target 'PageControlFrameworkTests' do
     inherit! :search_paths
